b9d40ea09099a5d0baa9c30ac0025f6d860b9d4b
augustin cousin <augustincousin.pro@gmail.com>
2018-12-03 16:20:53 +0100

    validations

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,9 @@
 class Delivery < ApplicationRecord
+  validates :recipient_name, presence: true
+  validates :recipient_phone, presence: true, format: { with: /^(\+33\s[1-9]{8})|(0[1-9]\s{8})$/ }
+  validates :address, presence: true
+  validates :complete_before, presence: true
+  validates :complete_after, presence: true
+
   belongs_to :company
 end
