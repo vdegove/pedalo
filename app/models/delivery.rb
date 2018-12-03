74775c4bcf457263fa08c8dcaaeb25a93169221c
augustin cousin <augustincousin.pro@gmail.com>
2018-12-03 18:26:22 +0100

    validation_with for complete_after/before

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,9 +1,20 @@
+class CompleteValidation < ActiveModel::Validator
+  def validate(record)
+    if record.complete_before < record.complete_after || record.complete_after < DateTime.now
+      record.errors[:base] << "Start time has to be after now"
+    end
+  end
+end
+
+
 class Delivery < ApplicationRecord
   validates :recipient_name, presence: true
   validates :recipient_phone, presence: true, format: { with: /^(\+33\s[1-9]{8})|(0[1-9]\s{8})$/ }
   validates :address, presence: true
   validates :complete_before, presence: true
   validates :complete_after, presence: true
+
+  validates_with CompleteValidation
 
   belongs_to :company
 end
