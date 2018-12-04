4dc616b73768dd5028df2271669f61f620a0f2e1
augustin cousin <augustincousin.pro@gmail.com>
2018-12-04 11:59:02 +0100

    added seeds

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,14 +1,8 @@
 class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-<<<<<<< HEAD
-  validates :address, presence: true
-  validates :contact_name, presence: true
-  # validates :contact_phone, format: { with: /^(\+33\s[1-9]{8})|(0[1-9]\s{8})$/ }
-=======
   # validates :address, presence: true
   # validates :contact_name, presence: true
   validates :contact_phone, format: { with: /\A(?:(?:\+|00)33|0)*[1-9](?:[\s.-]*\d{2}){4}\z/, :allow_blank => true }
->>>>>>> origin/package-types
 
   has_many :deliveries
   has_many :users
