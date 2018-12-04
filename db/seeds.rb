398cd093db3f5754584e694329862c823ee32bca
Vincent Degove <vincent.degove@muage.net>
2018-12-04 10:56:56 +0100

    add basic seeds for companies and package types

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating company Brasserie Tout Schuss…"
+Company.create!( name: "Brasserie Tout Schuss")
+
+puts "Creating company Alias Vert"
+Company.create!( name: "Alias Vert")
+
+
+puts "Creating beer package types…"
+PackageType.create!( name: "Pack de 12 bières blondes 33 cl")
+PackageType.create!( name: "Pack de 12 bières ambrées 33 cl")
+PackageType.create!( name: "Pack de 6 bières IPA 50 cl")
+PackageType.create!( name: "Fut de 30 L de bière blonde")
+
+puts "Creating plants package types…"
+PackageType.create!(name: "Grande jardinière 80 cm × 20 cm")
+PackageType.create!(name: "Pot de fleur moyen")
+
