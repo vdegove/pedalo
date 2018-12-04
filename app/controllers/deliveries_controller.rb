174543c69ca88f8e99ae050c236dc1f44bbb2b0a
augustin cousin <augustincousin.pro@gmail.com>
2018-12-04 16:35:16 +0100

    past/today/upcoming deliveries

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -6,7 +6,20 @@ class DeliveriesController < ApplicationController
 
   def past
     today = DateTime.now
-    # @deliveries = Delivery.where('today > complete_before')
+    @deliveries = Delivery.where('complete_before < ?', today)
+    authorize @deliveries
+  end
+
+  def upcoming
+    today = DateTime.now
+    @deliveries = Delivery.where('complete_after > ?', today)
+    authorize @deliveries
+  end
+
+  def today
+    today = DateTime.yesterday + 1.day
+    tomorrow = DateTime.tomorrow
+    @deliveries = Delivery.where('complete_after > ? AND complete_after < ?', today, tomorrow)
     authorize @deliveries
   end
 
