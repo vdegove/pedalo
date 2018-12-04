384969b415980aac32d99be07edf345c1260baf5
Vincent Degove <vincent.degove@muage.net>
2018-12-04 17:51:29 +0100

    first version of CSV upload

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,7 +1,27 @@
+require 'csv'
+
 class DeliveriesController < ApplicationController
+  skip_after_action :verify_authorized, only: :new
   def new
   end
 
   def create
+    if params[:csv_file]
+      CSV.foreach(params[:csv_file].tempfile, headers: true) do |row|
+        delivery = current_user.company.deliveries.new(
+          recipient_name: row["recipient_name"],
+          recipient_phone: row["recipient_phone"],
+          address: row["address"],
+          complete_after: DateTime.parse(row["complete_after"]),
+          complete_before: DateTime.parse(row["complete_after"])
+          )
+        authorize(delivery)
+
+        if delivery.save
+          redirect_to root_path
+        else
+          render :new
+      end
+    end
   end
 end
