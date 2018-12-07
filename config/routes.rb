a92825f3b0af4a6b8a213469d68dbc55bf7ccab7
Vincent Degove <vincent.degove@muage.net>
2018-12-07 15:37:27 +0100

    Implement a webhook from Onfleet for task completion

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,8 @@ Rails.application.routes.draw do
       get 'today', to: "deliveries#today"
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Webooks
+  post '/webhooks/onfleet/:token/task-completed', to: 'onfleet_webhooks#task_completed'
+  get '/webhooks/onfleet/:token/task-completed', to: 'onfleet_webhooks#task_completed'
 end
