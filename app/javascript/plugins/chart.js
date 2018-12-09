6b1b3489ba8caa6e93c2efb6bb0868791b6905e2
augustin cousin <augustincousin.pro@gmail.com>
2018-12-09 17:41:47 +0100

    style chartJS

--- app/javascript/plugins/chart.js
+++ app/javascript/plugins/chart.js
@@ -12,15 +12,27 @@ const rest = ctx.dataset.rest
 
 var myChart = new Chart(ctx, {
     type: 'doughnut',
+    options: {
+      circumference: Math.PI,
+      rotation: Math.PI,
+      legend: {
+        position: 'bottom',
+        fullWidth: true,
+        labels: {
+          fontSize: 14
+        }
+      }
+
+    },
     data: {
     datasets: [{
         backgroundColor: [
-                    'rgba(255, 99, 132, 0.5)',
-                    'rgba(54, 162, 235, 0.5)',
-                    'rgba(255, 206, 86, 0.5)'
+                    'rgba(70, 160, 70, 0.5)',
+                    'rgba(250, 70, 70, 0.5)',
+                    'rgba(160, 160, 160, 0.2)'
                 ],
         // data: [livred, delayed, rest]
-        data: [3, 4, 6]
+        data: [6, 4, 6]
     }],
 
     // These labels appear in the legend and in the tooltips when hovering different arcs
@@ -33,53 +45,55 @@ var myChart = new Chart(ctx, {
 });
 
   var ctx2 = document.getElementById("myChart2");
-var myChart = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            label: '# of Votes',
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ]},
-            {
-            data: [4, 10, 6, 7, 1, 6],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+  var myChart = new Chart(ctx2, {
+      type: 'bar',
+      data: {
+          options: {
+            borderWidth: 1,
+          },
+          labels: ["Lundi", "Mardi", "mercredi", "Jeudi", "Vendredi", "Samedi"],
+          datasets: [{
+              label: 'Semaine en cours',
+              data: [12, 19, 2, 2, 2, 2],
+              backgroundColor: [
+                  'rgba(54, 162, 235, 0.5)',
+                  'rgba(54, 162, 235, 0.5)',
+                  'rgba(54, 162, 235, 0.5)',
+                  'rgba(54, 162, 235, 0.5)',
+                  'rgba(54, 162, 235, 0.5)',
+                  'rgba(54, 162, 235, 0.5)'
+              ]},
+              {
+              label: 'Semaine passée',
+              data: [4, 10, 5, 3, 7, 2],
+              type: 'line'
+          }]
+      },
+      options: {
+          barThickness: 'flex',
+          gridLines:{
+            offsetGridLines: false
+          },
+
+          legend: {
+          position: 'bottom',
+          fullWidth: true,
+          labels: {
+            fontSize: 14
+          }
+        },
+
+
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }],
+
+          }
+      }
+  });
 
 
 
