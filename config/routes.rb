7de8dcd74e64d8aa528720cf2a2b5c30838d8e87
Laura Ducos <laura.ducosfilippi@gmail.com>
2018-12-12 12:52:01 +0100

    colors, dashboard, sidebar, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,15 @@ Rails.application.routes.draw do
   authenticated :user do
       root to: 'deliveries#dashboard'
   end
-
-  root 'pages#home', as: :authenticated_root
+  root 'pages#home'
+  get "/pages/home", to: "pages#home"
   # root to: 'deliveries#index', period: "today"
-
-
   get '/deliveries/bulk-new', to: 'deliveries#bulk_new'
   post 'deliveries/bulk-create', to: 'deliveries#bulk_create'
-
   get '/deliveries', to: 'deliveries#index', as: 'deliveries'
   resources :deliveries, only: [:create, :update, :show]
   resources :companies, only: [:new, :create]
-
   get '/dashboard', to: 'deliveries#dashboard'
-
-
   # resources :deliveries do
   #   collection do
   #     get 'phone', to: "deliveries#phone"
@@ -28,7 +22,6 @@ Rails.application.routes.draw do
   #     get 'photo', to: "deliveries#photo"
   #   end
   # end
-
   # Webooks
   post '/webhooks/onfleet/:token/task-completed', to: 'onfleet_webhooks#task_completed'
   get '/webhooks/onfleet/:token/task-completed', to: 'onfleet_webhooks#task_completed'
